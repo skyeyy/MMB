2ba8d9d5648dbde834eb92af1e6e9278f5153799
biaobiaozhu <zbbyeahnet@163.com>
2018-11-28 15:19:28 +0800

    比较搜索三级目录第一次提交

--- mmb/js/base.js
+++ mmb/js/base.js
@@ -1,10 +1,10 @@
 $(function(){
-    $.ajax ({
-        url: "http://localhost:9090/api/getindexmenu",
-        success: function(obj){
-            obj.result.unshift({name:"首页",titlehref:"index.html"});
-           var html = template("navTpl",obj);
-           $('.nav ul').html(html);
-        }
-    });
+    // $.ajax ({
+    //     url: "http://localhost:9090/api/getindexmenu",
+    //     success: function(obj){
+    //         obj.result.unshift({name:"首页",titlehref:"index.html"});
+    //        var html = template("navTpl",obj);
+    //        $('.nav ul').html(html);
+    //     }
+    // });
 })
